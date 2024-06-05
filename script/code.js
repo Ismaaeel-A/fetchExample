b674b5bf40ceb4aab4e53991ba8f9e3e4eceaa4b
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-06-05 16:28:31 +0200

    c

--- script/code.js
+++ script/code.js
@@ -27,19 +27,11 @@
 fetchData() */
 
 
+
 async function fetchData() {
     let res = await fetch("https://ismaaeel-a.github.io/onlineStore/data")
     let tafel = document.querySelector('[data-container]')
 
-    /*     let { results } = await res.json()
-        results.forEach(product => {
-            container.innerHTML += `
-            <div class='card'>
-                <img src='${product.img_url}'>
-                <button type="button">'${product.amount}'</button>
-                <p>'${product.description}'</p>
-            </div>
-        ` */
     let {
         results
     } = await res.json()
@@ -71,5 +63,64 @@ async function fetchData() {
     });
 
     console.log(results)
+
+    /*     let { results } = await res.json()
+    results.forEach(product => {
+        container.innerHTML += `
+        <div class='card'>
+            <img src='${product.img_url}'>
+            <button type="button">'${product.amount}'</button>
+            <p>'${product.description}'</p>
+        </div>
+    ` */
+
 }
 fetchData()
+
+function displayProducts(products) {
+    let container = document.querySelector('[data-container]');
+    container.innerHTML = ''; }
+
+async function searchFilter() {
+    let res = await fetch("https://ismaaeel-a.github.io/onlineStore/data")
+    let tafel = document.querySelector('[data-container]')
+    let search = document.querySelector('[searchbar]').value
+    let container = document.querySelector('[data-container]');
+
+    container.innerHTML = ''; 
+
+    let {
+        results
+    } = await res.json()
+    results.forEach(product => 
+        { if (search == `${product.catergory}`)
+            {
+                tafel.innerHTML +=
+                `
+        <tr>
+            <td>
+                <img src=${product.img_url}>
+            </td>
+    
+            <td>
+                <p>${product.catergory}</p>
+            </td>
+    
+            <td>
+                <p>${product.ProductName}</p>
+            </td>
+            
+            <td>
+                <p>${product.amount}</p>
+            </td>
+    
+            <td>
+                <p>${product.description}</p>
+            </td>
+    
+        </tr>    
+            `
+            }
+       
+    });
+}
